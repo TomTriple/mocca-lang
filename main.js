a2e4b220b69fb0ff1962c57e1b958cf77be1d7a9
thoefer <mail@tomhoefer.de>
2012-12-05 09:41:27 +0100

    refactored the test setup

---
+++ main.js
@@ -0,0 +1,19 @@
+(function() {
+
+  $(document).ready(function() {
+    var input = $("script[type='text/mocca']").text();
+    var cstream = new org.antlr.runtime.ANTLRStringStream(input);
+    var lexer = new Ex1TestLexer(cstream);
+    var tstream = new org.antlr.runtime.CommonTokenStream(lexer);
+    var parser = new Ex1TestParser(tstream);
+    var r = parser.prog();
+    // console.debug(r.getTree().toStringTree());
+
+    var nodes = new org.antlr.runtime.tree.CommonTreeNodeStream(r.getTree());
+    nodes.setTokenStream(tstream);
+    var walker = new Ex1Walker(nodes);
+    walker.prog().interpret();  
+  });
+
+}());
+
